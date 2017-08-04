6b5e48b8292e152e6f2def21166196a92a488d66
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    FWB: Sensor block per-package switch (1/2)
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6493,6 +6493,13 @@ public final class Settings {
         public static final String SWIPE_TO_SCREENSHOT = "swipe_to_screenshot";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        @Readable
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
