aefc5c5d5804a550a3f62e7b2b2d836d8b613c78
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-08 11:52:38 -0300

    fwb: Import SwipeToScreenshot implementation from MIUI
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    commit 827837b0e2deb21587ac24be99ac98e5828fb400 (HEAD -> ten-plus)
    Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Date:   Wed Oct 9 15:07:19 2019 +0000
    
        PhoneWindowManager: Disable three finger to screenshot on doze
    
        Change-Id: Ic7b38ffbe1980e78f9573dc2b0dc0b850d2c3ec2
    
    Change-Id: Iffdff674ba3419126df22150b47d1cb765b000d7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6487,6 +6487,12 @@ public final class Settings {
         public static final String CONFIG_ONGOING_PROGRESSBAR = "config_enableOngoingProgressBarChip";
 
         /**
+         * Swipe to screenshot
+         * @hide
+         */
+        public static final String SWIPE_TO_SCREENSHOT = "swipe_to_screenshot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6625,6 +6631,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(MOUSE_REVERSE_VERTICAL_SCROLLING);
             PRIVATE_SETTINGS.add(MOUSE_SWAP_PRIMARY_BUTTON);
             PRIVATE_SETTINGS.add(PREFERRED_REGION);
+            PRIVATE_SETTINGS.add(SWIPE_TO_SCREENSHOT);
         }
 
         /**
