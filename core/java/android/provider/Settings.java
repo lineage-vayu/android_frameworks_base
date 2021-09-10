d999c553af362603f5980a90e368f225be388d98
Adithya <gh0strider.2k18.reborn@gmail.com>
2021-09-10 14:20:54 +0530

    SystemUI: Show daily data usage in QS footer [SQUASHED]
     * Refactor existing build text view
     * Adapt to T
     * Improve and use controller
     * Hide when there is no wifi or sim
    
    Squashed:
    
     * SettingsLib: Add method to get daily data usage
     * SystemUI: Show wifi usage in QS footer when available
     * SystemUI: Show QS data usage suffix only when necessary
     * SystemUI: QS footer data usage improvements and cleanup
     * SystemUI: Allow hiding QS footer data usage [1/2]
     * SystemUI: Avoid NPE in QS footer data usage
     * SystemUI: Always show suffix for DataUsage
     * SystemUI: Improve QS data usage info
     * SystemUI: Debounce QS data usage updates
     * SystemUI: Set initial usage text view as invisible
     * SystemUI: Disable wifi listening in QS footer when view detached
     * SystemUI: Queue data usage text request only if enabled
     * SystemUI: Rework QS data usage UI
     * SystemUI: Fix janks in QS Data Usage when expanding QS Panel
    
    Change-Id: Ifcd39167e6522935a8a29dc89d7e5f415893d266
    Co-authored-by: AnkitGourav <Ankitgourav035@gmail.com>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6504,6 +6504,8 @@ public final class Settings {
          */
         public static final String SWIPE_TO_SCREENSHOT = "swipe_to_screenshot";
 
+        public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
